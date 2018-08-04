073c386ff78017d5153b3af23309cbe895140a1c
Gleidson FerSanP <gleidson@MacBook-Pro-de-Gleidson-FerSanP.local>
2018-08-04 00:11:34 -0300

    add page cart

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CartPage } from '../cart/cart';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,12 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  openCart(cartName: String){
+
+    console.log(cartName);
+    this.navCtrl.push(CartPage);
   }
 
 }
